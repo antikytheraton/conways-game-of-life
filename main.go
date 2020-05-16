c3050647ccd7c6505cc6ab7103b00075f4f651e1
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2020-05-16 12:02:39 -0500

    refactor(graphic): add module graphic
    BREAKING CHANGE: move opengl code to graphic module

--- main.go
+++ main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"runtime"
-	"strings"
+
+	"github/antikytheraton/conways-game-of-life/graphic"
 
 	"github.com/go-gl/gl/v4.6-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
-)
-
-const (
-	width              = 500
-	height             = 500
-	vertexShaderSource = `
-		#version 410
-		in vec3 vp;
-		void main() {
-			gl_Position = vec4(vp, 1.0);
-		}
-	` + "\x00"
-	fragmentShaderSource = `
-		#version 410
-		out vec4 frag_colour;
-		void main() {
-			frag_colour = vec4(1, 1, 1, 1);
-		}
-	` + "\x00"
 )
 
 var (
@@ -40,20 +20,40 @@ var (
 		0.5, -0.5, 0,
 	}
 	shapeCount = int32(len(square) / 3)
+
+	rows    = 10
+	columns = 10
 )
+
+type cell struct {
+	drawable uint32
+
+	x int
+	y int
+}
 
 func main() {
 	runtime.LockOSThread()
 
-	window := initGlfw()
+	window := graphic.InitGlfw()
 	defer glfw.Terminate()
-	program := initOpenGl()
+	program := graphic.InitOpenGL()
 
 	vao := makeVao(square)
 	for !window.ShouldClose() {
 		draw(vao, shapeCount, window, program)
 	}
 }
+
+// func makeCells() [][]*cell {
+// 	cells := make([][]*cell, rows, rows)
+// 	for x := 0; x < rows; x++ {
+// 		for y := 0; y < columns; y++ {
+// 			c := newCell(x, y)
+// 			cells[x] = append(cells[x], c)
+// 		}
+// 	}
+// }
 
 func draw(vao uint32, count int32, window *glfw.Window, program uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
@@ -64,51 +64,6 @@ func draw(vao uint32, count int32, window *glfw.Window, program uint32) {
 
 	glfw.PollEvents()
 	window.SwapBuffers()
-}
-
-// initGlfw initializes glfw and returns a Window to use
-func initGlfw() *glfw.Window {
-	if err := glfw.Init(); err != nil {
-		panic(err)
-	}
-
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	window, err := glfw.CreateWindow(width, height, "Conway's game of life", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-	window.MakeContextCurrent()
-
-	return window
-}
-
-// InitOpenGL initializes OpenGl and returns an initialized program.
-func initOpenGl() uint32 {
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	log.Println("OpenGL version", version)
-
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	prog := gl.CreateProgram()
-	gl.AttachShader(prog, vertexShader)
-	gl.AttachShader(prog, fragmentShader)
-	gl.LinkProgram(prog)
-	return prog
 }
 
 // makeVao (Vertex Array Object) initializes and returns a vertex
@@ -127,27 +82,4 @@ func makeVao(points []float32) uint32 {
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
 
 	return vao
-}
-
-func compileShader(source string, shaderType uint32) (uint32, error) {
-	shader := gl.CreateShader(shaderType)
-
-	csources, free := gl.Strs(source)
-	gl.ShaderSource(shader, 1, csources, nil)
-	free()
-	gl.CompileShader(shader)
-
-	var status int32
-	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
-	}
-
-	return shader, nil
 }
